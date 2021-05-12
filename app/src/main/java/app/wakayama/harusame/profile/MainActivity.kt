92a67142dbe8eca6ad46a11979c8f820c5c2a186
Haru39M <s2071149@s.konan-u.ac.jp>
2021-05-13 08:57:34 +0900

    add ver1,2,3,4

--- app/src/main/java/app/wakayama/harusame/profile/MainActivity.kt
+++ app/src/main/java/app/wakayama/harusame/profile/MainActivity.kt
@@ -2,10 +2,20 @@ package app.wakayama.harusame.profile
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.isVisible
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        profileImage.isVisible = false
+        profileCommentText.isVisible = false
+
+        button1.setOnClickListener {
+            profileImage.isVisible = true
+            profileCommentText.isVisible = true
+        }
     }
 }
